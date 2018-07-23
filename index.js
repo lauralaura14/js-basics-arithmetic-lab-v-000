726aae2931fd5292a307503441beb11b2de2c1fb
Laura <laurao148@gmail.com>
2018-07-23 04:35:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ const newID = 1000000000 + oldID;
 const ageIsValid = Number.isInteger(currentAge);
 const randomNumber = Math.floor(Math.random() * 20);
 const randomInteger = Math.round(randomNumber);
-const randomUserID = randomInteger ** 1000
+const randomUserID = randomInteger + 1000000000
